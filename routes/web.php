8f98969b7e82e9f30bc93e715a00e533beba3f29
mwangea <mwangeamusa @gmail.com>
2025-04-24 15:10:51 +0300

    feat: implement core exam functionality

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ExamController::class, 'index'])->name('exams.index');
 Route::get('/exams/{exam}', [ExamAttemptController::class, 'show'])->name('exams.attempt');
 Route::post('/exams/{exam}', [ExamAttemptController::class, 'store'])->name('exams.submit');
+
